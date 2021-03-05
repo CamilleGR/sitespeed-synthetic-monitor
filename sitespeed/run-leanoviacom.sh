eafd2c930a434e34dedabb49d5012f0efd6f9dac
camillegr <camille.gerin-roze@leanovia.com>
2021-03-06 00:38:51 +0100

    Ajout d'une boucle infini et amélioration de la dashboard principale

--- sitespeed/run-leanoviacom.sh
+++ sitespeed/run-leanoviacom.sh
@@ -1,1 +1,8 @@
-sitespeed.io -n 1 --headless --browsertime.chrome.collectTracingEvents --chrome.chromedriverPath ./chromedriver  --graphite.httpPort $(minikube service graphite-http --url | grep -Eo  '([0-9]+)$') --graphite.port $(minikube service graphite-api --url | grep -Eo  '([0-9]+)$') --graphite.host $(minikube ip)   https://www.leanovia.com/fr/
+#!/bin/bash
+
+while :
+do
+
+sitespeed.io -n 1 --headless --browsertime.chrome.collectTracingEvents --chrome.collectNetLog --chrome.chromedriverPath ./chromedriver  --graphite.httpPort $(minikube service graphite-http --url | grep -Eo  '([0-9]+)$') --graphite.port $(minikube service graphite-api --url | grep -Eo  '([0-9]+)$') --graphite.host $(minikube ip)   https://www.leanovia.com/fr/
+
+done
